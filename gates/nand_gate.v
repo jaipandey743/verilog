84303142f3b4cc9e29efdca7ecc93bb3ac032dc8
Jai Pandey <jaipandey743@gmail.com>
2026-03-24 14:19:20 +0530

    made a change

--- gates/nand_gate.v
+++ gates/nand_gate.v
@@ -1,4 +1,4 @@
-module nand_gate(input a, input b,input y);
-    assign out=~(a & b);
+module nand_gate(input a, input b,output y);
+    assign y=~(a & b);
     // ~ this is the sign of not
 endmodule
